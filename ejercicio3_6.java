f750dd6d64c31b9d2433c6d1092ed49cc9c57b32
matias1936 <116595648+matias1936@users.noreply.github.com>
2022-11-28 14:41:14 -0300

    ✌

---
+++ ejercicio3_6.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package bucles6al0;
+
+/**
+ *
+ * @author pc
+ */
+public class ejercicio3_6 {
+    
+}
